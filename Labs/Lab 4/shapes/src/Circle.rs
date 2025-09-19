531d454ec23b498eafc19ee1da7d8c3d34a82de6
Blessing <babumere04@gmail.com>
2025-09-18 20:13:41 -0400

    Create Circle.rs

---
+++ Labs/Lab 4/shapes/src/Circle.rs
@@ -0,0 +1,13 @@
+struct Circle {
+    radius: f64, // assume radius > 0
+}
+
+impl Circle {
+    const PI: f64 = 3.14;
+    fn new(radius: f64) -> Circle {
+        Circle { radius};
+    }
+
+    fn diameter
+
+}
