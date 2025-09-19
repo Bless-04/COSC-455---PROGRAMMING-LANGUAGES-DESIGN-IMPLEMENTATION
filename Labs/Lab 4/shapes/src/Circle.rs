f33f3b098052b86792eced3f543e9a7728cdcecd
Blessing <babumere04@gmail.com>
2025-09-18 20:17:32 -0400

    Update Circle.rs

--- Labs/Lab 4/shapes/src/Circle.rs
+++ Labs/Lab 4/shapes/src/Circle.rs
@@ -4,10 +4,27 @@ struct Circle {
 
 impl Circle {
     const PI: f64 = 3.14;
+
     fn new(radius: f64) -> Circle {
-        Circle { radius};
+        Circle { radius}
     }
 
-    fn diameter
+    fn area(&self) -> f64 {
+        return Circle::PI * (self.radius * self.radius);
+    }
 
+    fn circumference(&self) -> f64 {
+        return 2.0 * Circle::PI* self.radius;
+    }
+
+}
+
+
+fn main(){
+    let circle = Circle::new(5.0);
+
+    println!("Area: {}", circle.area());
+    println!("Circumference: {}", circle.circumference());
+    assert_eq!(circle.area(), 78.5);
+    assert_eq!(circle.circumference(), 31.400000000000002);
 }
