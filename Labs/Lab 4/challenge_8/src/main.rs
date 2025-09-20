b3660dc9909a08b3417449aac2cf6810c9897192
Blessing <babumere04@gmail.com>
2025-09-19 21:34:13 -0400

    Update main.rs
    challenge 8 working

--- Labs/Lab 4/challenge_8/src/main.rs
+++ Labs/Lab 4/challenge_8/src/main.rs
@@ -1,17 +1,32 @@
 use rand::Rng;
 use std::io;
 fn main() {
-
-    let N: u8  = rand::rng().random_range(1..=5);       // Create a random number generator
-
+    let N: u8  = rand::rng().random_range(1..=100);
     let mut buffer = String::new();
+    let mut guessed_number: u8; // assigning a default value makes a (never read) warning appear
 
-    while guessed_number != N{
+    loop {
+        println!("Enter a number between 1 and 100");
+        io::stdin()
+            .read_line(&mut buffer)
+            .expect("Failed to read input");
 
-        io::stdin().read_line()
+        guessed_number = buffer
+            .trim()
+            .parse::<u8>()
+            .expect("Input is not a valid number");
 
+        if guessed_number == N {
+            println!("You guessed the number.");
+            break;
+        }
 
+        if guessed_number < N {
+            println!("{} is too low",guessed_number);
+        } else {
+            println!("{} is too high",guessed_number);
+        }
+
+        buffer.clear();
     }
-
-
 }
