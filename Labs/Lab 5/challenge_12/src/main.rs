ed7fe56e5469424f9601061c8c369a8440220ac5
Blessing <babumere04@gmail.com>
2025-09-28 22:06:05 -0400

    finish 12

--- Labs/Lab 5/challenge_12/src/main.rs
+++ Labs/Lab 5/challenge_12/src/main.rs
@@ -1,9 +1,19 @@
+use std::fmt;
+
 struct Satellite {
     name: String,
     velocity: f64, // miles per second
 }
 
-/* YOUR CODE GOES HERE */
+impl fmt::Display for Satellite {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            fmt,
+            "Satellite '{}' has a velocity value of {} miles per second",
+            self.name, self.velocity
+        )
+    }
+}
 
 fn main() {
     let hubble = Satellite {
