b9034987e77cd4fb4ffbe7354254369843e6a9e5
Blessing <babumere04@gmail.com>
2025-10-30 20:15:20 -0400

    Update main.rs

--- Projects/P1/lolcompiler/src/main.rs
+++ Projects/P1/lolcompiler/src/main.rs
@@ -1,4 +1,7 @@
-use lolcompiler::parser::compiler::LolCompiler;
+use lolcompiler::parser::{
+    compiler::LolCompiler, lexer::LolLexer, syntax_analyzer::SyntaxAnalyzer,
+};
+
 use std::env;
 use std::fs;
 fn main() {
@@ -6,16 +9,21 @@ fn main() {
     if args.len() < 2 {
         // 2 is filename
         eprintln!("Usage: lolcompiler <filepath>");
-
-        //std::process::exit(1); uncomment when finished
+        //std::process::exit(1);
     }
     let path = &args[1];
-    let contents = fs::read_to_string(path).expect("Could not read file");
+
+    if !path.ends_with(".lol") {
+        eprintln!("Error: The file given is not a .lol file.");
+        //  std::process::exit(1);
+    }
+
+    let text = fs::read_to_string(path).expect(&format!("Failed to read '{}'", path));
     let compiler = LolCompiler::new();
-    println!("filepath: {} [{}]", path, contents.len());
+    println!("filepath: {} [{}]", path, text.len());
     //let mut parser = SyntaxAnalyzer::new(&mut lexer, &mut compiler);
     /*
-        parser.lolspeak();
+
         if !lexer.tokens.is_empty() || !compiler.current_token.is_empty() {
             eprintln!("A syntax error was encountered. Additional tokens found after the sentence.");
             std::process::exit(1);
