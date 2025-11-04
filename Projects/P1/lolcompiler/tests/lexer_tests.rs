7fe7fdb3e3384dc3fc4bc854a0b0970c20f56c96
Blessing <babumere04@gmail.com>
2025-11-04 17:03:01 -0500

    Update lexer_tests.rs

--- Projects/P1/lolcompiler/tests/lexer_tests.rs
+++ Projects/P1/lolcompiler/tests/lexer_tests.rs
@@ -2,20 +2,16 @@ use lolcompiler::compilation::lexer::LolLexer;
 
 #[cfg(test)]
 mod lexer_tests {
-
+    use super::LolLexer;
     fn read_file_to_string(path: &str) -> String {
         std::fs::read_to_string(path).expect(&format!("Failed to read '{}'", path))
     }
-
-    use lolcompiler::compilation::lexer;
-
-    use super::LolLexer; //makes it work??
 
     #[test]
     /// compiler only accepts .lol files.
     fn test() {
         let mut lexer = LolLexer::new("tttt");
 
-        println!("{:?}d", lexer.tokenize());
+        println!("{:?}", lexer);
     }
 }
