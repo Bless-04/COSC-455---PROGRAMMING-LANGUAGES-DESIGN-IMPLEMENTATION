785a4302509ba8990a6ba6669a3be72e7a8fa89b
Blessing <babumere04@gmail.com>
2025-11-04 16:58:06 -0500

    Update token.rs
    BNF fix

--- Projects/P1/lolcompiler/src/compilation/token.rs
+++ Projects/P1/lolcompiler/src/compilation/token.rs
@@ -1,10 +1,11 @@
 // Token enum for lolcode tokens
 #[derive(Debug)]
 pub enum Token {
-    Keyword(&'static str), // single keyword
+    Keyword(&'static str), // single keyword; keywords start with "#"
 
-    // literal string of something
-    Literal(&'static str),
+    VarDef(&'static str),   //any single word (A–Z, a–z, no spaces)
+    VarValue(&'static str), //allowed text characters
+    Address(&'static str),  //text without spaces ; should come after things like sound and video
 
     // variable name
     Identifier(&'static str),
