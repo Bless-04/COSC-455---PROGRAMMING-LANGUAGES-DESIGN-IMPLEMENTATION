7d1c2d985c68c960ec75ef54bd27c6f5681811cf
Blessing <babumere04@gmail.com>
2025-11-05 10:34:50 -0500

    Update lexer.rs

--- Projects/P1/lolcompiler/src/compilation/lexer.rs
+++ Projects/P1/lolcompiler/src/compilation/lexer.rs
@@ -2,61 +2,80 @@
 
 use crate::{LexicalAnalyzer, compilation::token::Token};
 use std::str::CharIndices;
-static KEYWORDS: [&'static str; 16] = [
+
+static KEYWORDS: [&'static str; 21] = [
     "#HAI",
     "#KTHXBYE",
     "#OBTW",
     "#TLDR",
-    "#MAEK HEAD",
+    "#MAEK",
     "#OIC",
-    "#GIMMEH TITLE",
+    "#GIMMEH",
     "#MKAY",
-    "#MAEK PARAGRAF",
-    "#GIMMEH BOLD",
-    "#GIMMEH ITALICS",
-    "#MAEK LIST",
-    "#GIMMEH ITEM",
-    "#GIMMEH NEWLINE",
-    "#GIMMEH SOUNDZ",
-    "#GIMMEH VIDZ",
+    "HEAD",
+    "TITLE",
+    "PARAGRAF",
+    "BOLD",
+    "ITALICS",
+    "LIST",
+    "ITEM",
+    "NEWLINE",
+    "SOUNDZ",
+    "VIDZ",
+    "#I HAZ",
+    "#IT IZ",
+    "#LEMME SEE",
 ];
 
+#[derive(Debug)]
 // Lexical Analyzer for lolcode ; first step of compiling ; 1
 pub struct LolLexer<'a> {
-    _input: CharIndices<'a>, //source text
+    _text: CharIndices<'a>, //source text
     _tokens: Vec<Token>,
     _potential_token: String,
 }
 
 impl<'a> LolLexer<'a> {
+    fn is_keyword(s: &str) -> bool {
+        KEYWORDS.iter().any(|k| k.eq_ignore_ascii_case(s))
+    }
+
+    fn is_ws(c: char) -> bool {
+        c != '\n' || c.is_whitespace()
+    }
     pub fn new(text: &'a str) -> Self {
         Self {
             _potential_token: String::new(),
-            _input: text.char_indices(), // rust char iterator that references text
+            _text: text.char_indices(), // rust char iterator that references text
             _tokens: Vec::new(),
         }
     }
+
     pub fn tokens(&self) -> &Vec<Token> {
         &self._tokens
     }
-}
-/*
-pub fn start(&mut self) -> String {
-    let candidate_token = self.tokens.pop().unwrap_or_default();
+    pub fn tokenize(&mut self) {
 
-    if self.lookup(&candidate_token) {
-        candidate_token
-    } else if !candidate_token.is_empty() {
-        eprintln!(
-            "A lexical error was encountered. '{}' is not a recognized token.",
+        /*
+        let candidate_token = self.tokens.pop().unwrap_or_default();
+
+        if self.lookup(&candidate_token) {
             candidate_token
-        );
-        std::process::exit(1);
-    } else {
-        eprintln!("A user error was encountered. The provided sentence is empty.");
-        std::process::exit(1);
+        } else if !candidate_token.is_empty() {
+            eprintln!(
+                "A lexical error was encountered. '{}' is not a recognized token.",
+                candidate_token
+            );
+            std::process::exit(1);
+        } else {
+            eprintln!("A user error was encountered. The provided sentence is empty.");
+            std::process::exit(1);
+        }
+        */
     }
 }
+/*
+
 
 pub fn next(&mut self) -> String {
     let candidate_token = self.tokens.pop().unwrap_or_default();
@@ -81,12 +100,13 @@ pub fn is_a_article(&self, word: &str) -> bool {
 
 impl LexicalAnalyzer for LolLexer<'_> {
     fn get_char(&mut self) -> char {
-        let result = self._input.next();
+        let result = self._text.next();
 
         match result {
-            Some((p, c)) => c,
+            Some((_, c)) => c,
             None => '\0',
         }
+        //todo: terminate program if exhausted
     }
 
     fn add_char(&mut self, c: char) {
@@ -97,6 +117,6 @@ impl LexicalAnalyzer for LolLexer<'_> {
     ///returns true if its valid
     /// false otherwise
     fn lookup(&self, s: &str) -> bool {
-        KEYWORDS.iter().any(|k| k.eq_ignore_ascii_case(s))
+        Self::is_keyword(s)
     }
 }
