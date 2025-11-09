b0bf1e521b72fdbaef67c8399d2431117b1e8425
Blessing <babumere04@gmail.com>
2025-11-09 18:14:26 -0500

    Update lexer_tests.rs

--- Projects/Project 1/lolcompiler/tests/lexer_tests.rs
+++ Projects/Project 1/lolcompiler/tests/lexer_tests.rs
@@ -2,6 +2,8 @@ use lolcompiler::compilation::lexer::LolLexer;
 
 #[cfg(test)]
 mod lexer_tests {
+    use lolcompiler::LexicalAnalyzer;
+
     use super::LolLexer;
     fn read_file_to_string(path: &str) -> String {
         std::fs::read_to_string(path).expect(&format!("Failed to read '{}'", path))
@@ -13,5 +15,15 @@ mod lexer_tests {
         let mut lexer = LolLexer::new("tttt");
 
         println!("{:?}", lexer);
+    }
+
+    #[test]
+    fn test_lookup() {
+        let lexer = LolLexer::new("tttt");
+
+        assert_eq!(lexer.lookup("#HAI"), true);
+        assert_eq!(lexer.lookup("#hAi"), true); //case insensitive 1
+        assert_eq!(lexer.lookup("#hai"), true); //case insensitive 2
+        assert_eq!(lexer.lookup("random text"), false);
     }
 }
