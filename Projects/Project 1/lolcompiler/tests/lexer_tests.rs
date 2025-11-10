d4ed2c5175a9e5b21ec3f35ed1a916a50c7e7752
Blessing <babumere04@gmail.com>
2025-11-09 20:42:59 -0500

    Update lexer_tests.rs

--- Projects/Project 1/lolcompiler/tests/lexer_tests.rs
+++ Projects/Project 1/lolcompiler/tests/lexer_tests.rs
@@ -5,6 +5,7 @@ mod lexer_tests {
     use lolcompiler::LexicalAnalyzer;
 
     use super::LolLexer;
+
     fn read_file_to_string(path: &str) -> String {
         std::fs::read_to_string(path).expect(&format!("Failed to read '{}'", path))
     }
@@ -12,18 +13,21 @@ mod lexer_tests {
     #[test]
     /// compiler only accepts .lol files.
     fn test() {
-        let mut lexer = LolLexer::new("tttt");
-
-        println!("{:?}", lexer);
+        println!("{:?}", LolLexer::new("#OBTW comment #TLDR"));
     }
 
     #[test]
     fn test_lookup() {
-        let lexer = LolLexer::new("tttt");
+        let lexer = LolLexer::new("lexer");
 
-        assert_eq!(lexer.lookup("#HAI"), true);
-        assert_eq!(lexer.lookup("#hAi"), true); //case insensitive 1
-        assert_eq!(lexer.lookup("#hai"), true); //case insensitive 2
-        assert_eq!(lexer.lookup("random text"), false);
+        assert!(
+            lexer.lookup("variable"),
+            "'variable' should be a valid variable name"
+        );
+
+        assert!(
+            !lexer.lookup("123variable"),
+            "'123variable' should NOT be a valid variable name"
+        );
     }
 }
