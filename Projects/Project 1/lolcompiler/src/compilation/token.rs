f71753a914d202c5623448ae11ec521d6067f00c
Blessing <babumere04@gmail.com>
2025-11-09 19:01:16 -0500

    Update token.rs
    token rework

--- Projects/Project 1/lolcompiler/src/compilation/token.rs
+++ Projects/Project 1/lolcompiler/src/compilation/token.rs
@@ -1,16 +1,32 @@
 // Token enum for lolcode tokens
 #[derive(Debug)]
 pub enum Token<'a> {
-    Keyword(&'a str), // single keyword; keywords start with "#"
+    HAI,
+    KTHXBYE,
+    OBTW,
+    TLDR,
+    MAEK,
+    OIC,
+    GIMMEH,
+    MKAY,
+    HEAD,
+    TITLE,
+    PARAGRAF,
+    BOLD,
+    ITALICS,
+    LIST,
+    ITEM,
+    NEWLINE,
+    SOUNDZ,
+    VIDZ,
+    IHAZ,
+    ITIZ,
+    LEMMESEE,
+    VARDEF(&'a str), //any single word (A–Z, a–z, no spaces)
+    VARVAL(&'a str), //allowed text characters
 
-    VarDef(&'a str),   //any single word (A–Z, a–z, no spaces)
-    VarValue(&'a str), //allowed text characters
-    Address(&'a str),  //text without spaces ; should come after things like sound and video
-    // variable name
-    Identifier(&'a str),
-    // =
-    Operator(&'a str),
-    // <!-- -->
-    Comment(&'a str),
+    TEXT(&'a str),    //plain text (letters, digits, punctuation, spaces)
+    ADDRESS(&'a str), //text without spaces ; should come after things like sound and video
+
     END,
 }
