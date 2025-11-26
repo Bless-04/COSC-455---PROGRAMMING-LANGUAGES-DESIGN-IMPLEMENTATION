1ba3d09521fb86831301372aa7dff4eb75a77fc2
Blessing <babumere04@gmail.com>
2025-11-25 21:36:16 -0500

    Refactor translation logic and clean up main function
    Replaces dupe word-to-number matching with a translation map and helper functions for better readability

--- Projects/Project 2/5. Language Math Translation/lmt.rkt
+++ Projects/Project 2/5. Language Math Translation/lmt.rkt
@@ -1,100 +1,98 @@
 #lang racket
 
-;;; scheme comment
-;;; matches chinese or english word to number or #f
-(define (match-word input)
-  (cond
-    ((or (string-ci=? input "ling") (string-ci=? input "zero")) 0)
-    ((or (string-ci=? input "yi") (string-ci=? input "one")) 1)
-    ((or (string-ci=? input "er") (string-ci=? input "two")) 2)
-    ((or (string-ci=? input "san") (string-ci=? input "three")) 3)
-    ((or (string-ci=? input "si") (string-ci=? input "four")) 4)
-    ((or (string-ci=? input "wu") (string-ci=? input "five")) 5)
-    ((or (string-ci=? input "liu") (string-ci=? input "six")) 6)
-    ((or (string-ci=? input "qi") (string-ci=? input "seven")) 7)
-    ((or (string-ci=? input "ba") (string-ci=? input "eight")) 8)
-    ((or (string-ci=? input "jiu") (string-ci=? input "nine")) 9)
-    ((or (string-ci=? input "shi") (string-ci=? input "ten")) 10)
-    (else #f)))
 
-;;; n+...n = sum
+;;; From prev questions
+(define (first x) (car x))
+(define (second x) (cadr x))
+(define (rest x) (cdr x))
+
+;;; mapping the words to numbers
+(define translation-map
+  '(("ling" 0) ("zero" 0)
+    ("yi" 1)   ("one" 1)
+    ("er" 2)   ("two" 2)
+    ("san" 3)  ("three" 3)
+    ("si" 4)   ("four" 4)
+    ("wu" 5)   ("five" 5)
+    ("liu" 6)  ("six" 6)
+    ("qi" 7)   ("seven" 7)
+    ("ba" 8)   ("eight" 8)
+    ("jiu" 9)  ("nine" 9)
+    ("shi" 10) ("ten" 10)))
+
+
+;;; match-word: Matches word to number
+(define (match-word input)
+  (let ((found-pair (assoc (string-downcase input) translation-map)))
+    (if found-pair
+        (second found-pair) 
+        #f))) ; must have #f or problems
+
+;;; converts the words to numbers
+(define (translate-input input)
+  (cond
+    ((null? input) '()) ; empty list for basecase
+    (else
+     (let ((word (first input))) 
+       (let ((num (match-word word)))
+         (if num
+             ;;  keep  and process  rest if valid
+             (cons num (translate-input (rest input)))
+             ;; skip when not valid
+             (translate-input (rest input))))))))
+
 (define (print-sum lst sum)
   (if (null? lst)
       sum
-      (let* ((val (car lst))
+      (let* ((val (first lst))
              (new-sum (+ sum val)))
-        (if (null? (cdr lst))
+        (if (null? (rest lst))
             (begin
-              (display val)
-              (display " = ")
-              (display new-sum)
-              (newline)
+              (display val) (display " = ") (display new-sum) (newline)
               new-sum)
             (begin
-              (display val)
-              (display " + ")
-              (print-sum (cdr lst) new-sum))))))
+              (display val) (display " + ")
+              (print-sum (rest lst) new-sum))))))
 
-;;; n*...n = product
+
 (define (print-product lst product)
   (if (null? lst)
       product
-      (let* ((val (car lst))
+      (let* ((val (first lst))
              (new-product (* product val)))
-        (if (null? (cdr lst))
+        (if (null? (rest lst))
             (begin
-              (display val)
-              (display " = ")
-              (display new-product)
-              (newline)
+              (display val) (display " = ") (display new-product) (newline)
               new-product)
             (begin
-              (display val)
-              (display " * ")
-              (print-product (cdr lst) new-product))))))
+              (display val) (display " * ")
+              (print-product (rest lst) new-product))))))
 
-;;;  filters strings to numbers
-(define (translate-input input)
-  (if (null? input)
-      '()
-      (let ((number (match-word (car input))))
-        (if number
-            (cons number (translate-input (cdr input)))
-            (translate-input (cdr input))))))
-
+;;; the part that matters
 (define (go input)
-  (if (null? input)
-      '()
-      (let ((result (translate-input input)))
-        
-        (display "Translation: ")
-        (display result)
-        (newline)
+  (let ((result (translate-input input)))
+    
+    (display "Translation: ")
+    (display result)
+    (newline)
 
-        (if (not (null? result))
-            (begin
-               (display "Addition: ")
-               (print-sum result 0)
-               
-               (display "Multiplication: ")
-               (print-product result 1)
-               (newline))
-            #f) ;;; #f fixed the else issue
-        
-        result)))
-;;; exists to test
-(define (assert-eq expected actual message)
-  (if (equal? expected actual)
-      (begin 
-        (display "WORKING:") 
-        (display message) 
-        (newline))
-      (begin 
-        (display "FAILING: ") 
-        (display message) 
-        (display " | Expected: ") (display expected)
-        (display " Got: ") (display actual)
-        (newline))))
+    (if (not (null? result))
+        (begin
+          (display "Addition: ")
+          (print-sum result 0)
+          
+          (display "Multiplication: ")
+          (print-product result 1)
+          (newline))
+        #f)))
+
+;;; Testing
+
+(go '("yi" "nine" "six" "ba"))
+
+(go '("yi" "josh" "three" "si"))
+
+
 
 (define (main)
   (let ((input '("yi" "nine" "six" "ba")))
@@ -103,16 +101,4 @@
   (let ((input '("yi" "josh" "three" "si")))
     (go input))
 
-  (let ((numbers '(1 2 3 4 5 6 7 8 9 10)))
-    (assert-eq 
-     numbers
-     (go '("skip" "yi" "er" "san" "si" "wu" "liu" "qi" "ba" "jiu" "shi"))
-     "Chinese Check")
-
-    (assert-eq 
-     numbers
-     (go '("skip" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten"))
-     "English Check")))
-
-;;;
-(main)
+  )
