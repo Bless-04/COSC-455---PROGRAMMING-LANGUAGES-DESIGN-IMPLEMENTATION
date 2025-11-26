8d0f432b34bb6832c5b08d25fce7e8da1387f684
Blessing <babumere04@gmail.com>
2025-11-25 21:03:42 -0500

    Update hitchhiker.rs
    more tests

--- Projects/Project 2/1. SML & Rust Warmup/hitchhiker.rs
+++ Projects/Project 2/1. SML & Rust Warmup/hitchhiker.rs
@@ -1,6 +1,11 @@
 fn main() {
-    println!(hitchhiker(&[1, 2, 3, 4, 5, 42, 6, 7, 8, 9, 10]));
-    println!(hitchhiker(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    println!("{}", hitchhiker(&[1, 2, 3, 4, 5, 42, 6, 7, 8, 9, 10]));
+    println!("{}", hitchhiker(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+    assert_eq!(hitchhiker(&[]), false);
+    assert_eq!(hitchhiker(&[42]), true);
+    assert_eq!(hitchhiker(&[1, 2, 3, 4, 5, 42, 6, 7, 8, 9, 10]), true);
+    assert_eq!(hitchhiker(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), false);
 }
 
 fn hitchhiker(list: &[i32]) -> bool {
