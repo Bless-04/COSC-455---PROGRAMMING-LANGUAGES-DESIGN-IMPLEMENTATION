5716536e1675bf40f1f09d92ba1043aa79971d6f
Blessing <babumere04@gmail.com>
2025-11-25 23:33:02 -0500

    Update 2. truecount.rkt

--- Projects/Project 2/4. Scheme Preparation/2. truecount.rkt
+++ Projects/Project 2/4. Scheme Preparation/2. truecount.rkt
@@ -2,7 +2,7 @@
 (define (truecount lst)
   (cond
     ((null? lst) 0) ;  empty  = 0
-    ((eq? (car lst) true) (+ 1 (truecount (cdr lst)))) ; true = +1
+    ((eq? (car lst) #t) (+ 1 (truecount (cdr lst)))) ; true = +1
     (else (truecount (cdr lst)))) ); false = continue
 
 (display (truecount '(#t #f #t #t #f))) ;should be 3
